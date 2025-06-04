70aa0c07bc815e2190cb16bb86b9d858ad5a37c2
yonash24 <yonashay87@gmail.com>
2025-06-04 22:53:45 +0300

    updating and finish Board class

--- Board.py
+++ Board.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 import Rook
 import Bishop
 import King
@@ -7,35 +6,87 @@ import Pawn
 import Knight
 
 class Board:
-    
+
     def __init__(self):
-        self.game_board = [
-            [Rook("black",0,0),Knight("black",0,1),Bishop("black",0,2),Queen("black",0,3),Knight("black",0,4),Bishop("black",0,5),Knight("black",0,6),Rook("black",0,7)],
-            [Pawn("black",1,0),Pawn("black",1,1),Pawn("black",1,2),Pawn("black",1,3),Pawn("black",1,4),Pawn("black",1,5),Pawn("black",1,6),Pawn("black",1,7)],
-            [None for _ in range(8)],
-            [None for _ in range(8)],
-            [None for _ in range(8)],
-            [None for _ in range(8)],
-            [Rook("white",6,0),Knight("white",6,1),Bishop("white",6,2),Queen("white",6,3),Knight("white",6,4),Bishop("white",6,5),Knight("white",6,6),Rook("white",6,7)],
-            [Pawn("white",7,0),Pawn("white",7,1),Pawn("white",7,2),Pawn("white",7,3),Pawn("white",7,4),Pawn("white",7,5),Pawn("white",7,6),Pawn("white",7,7)] 
+        self.game_board = [[None for _ in range(8)] for _ in range(8)]
+
+        # Setup Black Pieces
+        self.game_board[0] = [
+            Rook.Rook("black", 0, 0),
+            Knight.Knight("black", 0, 1),
+            Bishop.Bishop("black", 0, 2),
+            Queen.Queen("black", 0, 3),
+            King.King("black", 0, 4),
+            Bishop.Bishop("black", 0, 5),
+            Knight.Knight("black", 0, 6),
+            Rook.Rook("black", 0, 7)
         ]
+        for col in range(8):
+            self.game_board[1][col] = Pawn.Pawn("black", 1, col)
 
+        # Setup White Pieces
+        self.game_board[7] = [
+            Rook.Rook("white", 7, 0),
+            Knight.Knight("white", 7, 1),
+            Bishop.Bishop("white", 7, 2),
+            Queen.Queen("white", 7, 3),
+            King.King("white", 7, 4),
+            Bishop.Bishop("white", 7, 5),
+            Knight.Knight("white", 7, 6),
+            Rook.Rook("white", 7, 7)
+        ]
+        for col in range(8):
+            self.game_board[6][col] = Pawn.Pawn("white", 6, col)
 
-    def update_tool_location(self):
-        pass
-    
-    def is_empty(self,row, col, color):
-        if self.game_board[row][col] == '-' or self.game_board.color != color:
+    def is_empty(self, row, col, color=None):
+        piece = self.game_board[row][col]
+        if piece is None:
             return True
-        else:
+        if color is not None and piece.color == color:
             return False
-    
-    def get_king_position(self,color):
-        for i in range(7):
-            for j in range(7):
-                if isinstance(self.game_board[i][j],King) and self.game_board[i][j].color != color:
-                    return self.game_board[i][j].position
-                
-    
-        
-        
+        return False
+
+    def get_king_position(self, color):
+        for i in range(8):
+            for j in range(8):
+                piece = self.game_board[i][j]
+                if piece and isinstance(piece, King.King) and piece.color == color:
+                    return (i, j)
+        return None
+
+    def print_board(self):
+        for row in self.game_board:
+            row_str = ""
+            for piece in row:
+                if piece is None:
+                    row_str += "- "
+                else:
+                    symbol = type(piece).__name__[0]
+                    symbol = symbol.upper() if piece.color == "white" else symbol.lower()
+                    row_str += symbol + " "
+            print(row_str)
+        print()
+
+    def move_piece(self, start_row, start_col, end_row, end_col):
+        piece = self.game_board[start_row][start_col]
+        if piece is None:
+            print("No piece at source.")
+            return
+
+        if piece.color != self.turn:
+            print(f"It's {self.turn}'s turn.")
+            return
+
+        # Try to move or eat
+        target = self.game_board[end_row][end_col]
+        if target is None:
+            piece.move(end_row, end_col, self)
+        else:
+            piece.eat(end_row, end_col, self)
+
+        # Update board reference
+        self.game_board[start_row][start_col] = None
+        self.game_board[end_row][end_col] = piece
+
+    def get_piece(self, row, col):
+        return self.game_board[row][col]
